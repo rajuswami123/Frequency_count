8a1aefaa8001b5251eb6aef420a4da0d13cf6598
rajuswami123 <raju.swamireddy@tringapps.com>
2023-02-15 21:43:59 +0530

    all files via commited

--- src/main/java/org/example/WordFrequencyCounter.java
+++ src/main/java/org/example/WordFrequencyCounter.java
@@ -35,7 +35,7 @@ public class WordFrequencyCounter {
 
             scanner.close();
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            Logger.getGlobal();
         }
     }
 }
